cd3703485e2f1956830f95ae394599d575a32bee
AngryFender <teaBiscuitPerson@gmail.com>
2023-12-19 23:54:15 +0000

    Github Issue #2: Added low level keyboard event hook to track the keys presses Github Issue #3: Added timer of 1second to measure the total number of characters per second which will be converted to words per minute Github Issue #4: 3 different gifs will be shown for 3 different typing speed range Github Issue #5: Where applicable the gif and the wpm will be shown right next to the text caret. If not applicable then it will be shown at the bottom right corner of the corresponding window Github Issue #6: All gif and wpm will hidden when backspace is pressed. Instead a pacman eating the character will be shown as a gif Github Issue #7: The app has been moved to the system tray and an icon has been added to it

--- Faster/MainWindow.xaml.cs
+++ Faster/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Application = System.Windows.Application;
+using ContextMenu = System.Windows.Forms.ContextMenu;
 
 namespace Faster
 {
@@ -20,17 +25,110 @@ namespace Faster
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        private NotifyIcon _notifyIcon;
+        private Queue<double> _wpms = new Queue<double>();
         public MainWindow()
         {
             InitializeComponent();
-            WinMonitor.SetUIDispatcher(this.Dispatcher);
-            WinMonitor.GetInstance().WPMChanged += WPMChangedHandler;
+            
+            this._notifyIcon = new NotifyIcon
+            {
+                BalloonTipText = "Faster is minimized to tray",
+                BalloonTipTitle = "Faster",
+                Text = "Faster",
+                Icon = Properties.Resources.Faster,
+                Visible = true
+            };
+
+            ContextMenu trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", (s, e) => CloseApplication());
+            _notifyIcon.ContextMenu = trayMenu;
+            this.Loaded += (sender, e) =>
+            {
+                WindowInteropHelper helper = new WindowInteropHelper(this);
+                IntPtr hwnd = helper.Handle;
+                WinMonitor.GetInstance().WPMChanged += WPMChangedHandler;
+                WinMonitor.GetInstance().HideWPM += HideWPMHandler;
+                WinMonitor.GetInstance().BackSpaceKeyPressed += DeleteKeyPressedHandler;
+                WinMonitor.GetInstance().SetCurrentHWnd(hwnd);
+                WinMonitor.GetInstance().SetUIDispatcher(this.Dispatcher);
+            };
+            TxtWPM.Visibility = Visibility.Collapsed;
+            ImgLow.Visibility = Visibility.Collapsed;
+            ImgMid.Visibility = Visibility.Collapsed;
+            ImgFire.Visibility = Visibility.Collapsed;
+            ImgDelete.Visibility = Visibility.Collapsed;
+        }
+
+        private void DeleteKeyPressedHandler(object sender, bool e)
+        {
+            if (e)
+            {
+                TxtWPM.Visibility = Visibility.Collapsed;
+                ImgLow.Visibility = Visibility.Collapsed;
+                ImgMid.Visibility = Visibility.Collapsed;
+                ImgFire.Visibility = Visibility.Collapsed;
+                ImgDelete.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ImgDelete.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void HideWPMHandler(object sender, EventArgs e)
+        {
+            TxtWPM.Visibility = Visibility.Collapsed;
+            ImgLow.Visibility = Visibility.Collapsed;
+            ImgMid.Visibility = Visibility.Collapsed;
+            ImgFire.Visibility = Visibility.Collapsed;
+            ImgDelete.Visibility = Visibility.Collapsed;
+        }
+
+        private void CloseApplication()
+        {
+            Application.Current.Shutdown();
         }
 
         private void WPMChangedHandler(object sender, WPMArgs e)
         {
-            TxtWPM.Text = e.WPM.ToString();
+            TxtWPM.Visibility = Visibility.Visible;
+
+            _wpms.Enqueue(e.WPM);
+            if (_wpms.Count > 5)
+            {
+                _wpms.Dequeue();
+            }
+
+            double averageWpm = 0;
+            foreach (var wpm in _wpms)
+            {
+                averageWpm += wpm;
+            }
+            averageWpm = averageWpm / _wpms.Count;
+            TxtWPM.Text = averageWpm.ToString();
+
+            if(averageWpm >= 80 && averageWpm < 150)
+            {
+                ImgMid.Visibility = Visibility.Visible;
+                ImgLow.Visibility = Visibility.Collapsed;
+                ImgFire.Visibility = Visibility.Collapsed;
+                ImgDelete.Visibility = Visibility.Collapsed;
+            }
+            else if (averageWpm >= 150)
+            {
+                ImgFire.Visibility = Visibility.Visible;
+                ImgLow.Visibility = Visibility.Collapsed;
+                ImgMid.Visibility = Visibility.Collapsed;
+                ImgDelete.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ImgLow.Visibility = Visibility.Visible;
+                ImgMid.Visibility = Visibility.Collapsed;
+                ImgFire.Visibility = Visibility.Collapsed;
+                ImgDelete.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
--- Faster/WinMonitor.cs
+++ Faster/WinMonitor.cs
@@ -10,21 +10,23 @@ namespace Faster
     public class WinMonitor: IDisposable
     {
         private static WinMonitor instance;
-        private KeyboardProc _keyboardProc;
-        private IntPtr _keyboardEventHook;
+        private readonly KeyboardProc _keyboardProc;
+        private readonly IntPtr _keyboardEventHook;
         private static System.Timers.Timer _timer;
+        private static System.Timers.Timer _visibilityTimer;
         private static int _countKeys;
         private readonly int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
+        private const int VK_DELETE = 0x08;
+        private bool _isBackSpacePressed = false;
         private static Dispatcher _uiDispatcher;
+        private static IntPtr _currentHWnd;
+        private static WinRect _currentRect;
 
         private delegate IntPtr KeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool GetCaretPos(out Position pos);
 
         [DllImport("kernel32.dll")]
         static extern IntPtr LoadLibrary(string lpFileName);
@@ -45,15 +47,95 @@ namespace Faster
             , IntPtr wParam
             , IntPtr lParam);
 
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool MoveWindow(IntPtr hWnd,int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd,out WinRect lpRect);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("user32.dll")]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+
+        [DllImport("user32.dll")]
+        static extern bool GetGUIThreadInfo(uint threadId, ref GUITHREADINFO info);
+
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd,ref  Position lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct WinRect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+   
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Position
+        {
+            public int x;
+            public int y;
+
+            public Position(int x, int y)
+            {
+                this.x = x; this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct GUITHREADINFO
+        {
+            public int cbSize;
+            public int flags;
+            public IntPtr hwndActive;
+            public IntPtr hwndFocus;
+            public IntPtr hwndCapture;
+            public IntPtr hwndMenuOwner;
+            public IntPtr hwndMoveSize;
+            public IntPtr hwndCaret;
+            public WinRect rcCaret;
+        }
+
         private WinMonitor() 
         {
             _keyboardProc = new KeyboardProc(HandleKeyboardEvent);
 
              IntPtr hInstance = LoadLibrary("User32");
             _keyboardEventHook = SetWindowsHookEx(WH_KEYBOARD_LL, _keyboardProc, hInstance,  0);
+
             _timer = new System.Timers.Timer(1000);
             _timer.Elapsed += TimerElapsedHandler;
             _timer.AutoReset = true;
+
+            _visibilityTimer = new System.Timers.Timer(5000);
+            _visibilityTimer.Elapsed += VisibilityTimerElapsedHandler;
+            _visibilityTimer.AutoReset = true;
+        }
+
+
+        public event EventHandler HideWPM;
+
+        public void RaiseHideWPM()
+        {
+            if(null == _uiDispatcher)
+            {
+                return;
+            }
+            _uiDispatcher.Invoke(()=>{
+                HideWPM?.Invoke(this, EventArgs.Empty);
+            });
+        }
+
+        private void VisibilityTimerElapsedHandler(object sender, ElapsedEventArgs e)
+        {
+            RaiseHideWPM();
+            _visibilityTimer?.Stop();
         }
 
         public event EventHandler<WPMArgs> WPMChanged;
@@ -65,10 +147,15 @@ namespace Faster
                 return;
             }
 
+            float wpm = ((float)_countKeys * 60.0f) / (5.0f);
             _uiDispatcher.Invoke(() =>
             {
-                WPMChanged?.Invoke(this, new WPMArgs(_countKeys));
+                WPMChanged?.Invoke(this, new WPMArgs((double)wpm));
             });
+
+            _visibilityTimer.Stop();
+            _visibilityTimer.Start();
+            
         }
 
         private void TimerElapsedHandler(object sender, ElapsedEventArgs e)
@@ -78,20 +165,40 @@ namespace Faster
                 return;
             }
 
-            _timer.Stop();
+            _timer?.Stop();
             RaiseWPMChanged();
             Interlocked.Exchange(ref _countKeys, 0);
+        }
+
+        public event EventHandler<bool> BackSpaceKeyPressed;
+        public void RaiseBackKeyPressed(bool isPressed)
+        {
+            _visibilityTimer.Stop();
+            _visibilityTimer.Start();
+            BackSpaceKeyPressed?.Invoke(this, isPressed);
         }
 
         private IntPtr HandleKeyboardEvent(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if ( nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
-                if (!_timer.Enabled)
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == VK_DELETE)
                 {
-                    _timer.Start();
+                    RaiseBackKeyPressed(true);
+                    _isBackSpacePressed = true;
                 }
-                Interlocked.Increment(ref _countKeys);
+                else
+                {
+                    RaiseBackKeyPressed(false);
+
+                    if (!_timer.Enabled)
+                    {
+                        _timer.Start();
+                    }
+                    Interlocked.Increment(ref _countKeys);
+                }
+                GetCaretPosition();
             }
             return CallNextHookEx(_keyboardEventHook, nCode, wParam, lParam);
         }
@@ -105,21 +212,57 @@ namespace Faster
             return instance;
         }
 
-        public static void SetUIDispatcher(Dispatcher ui)
+        public void SetCurrentHWnd(IntPtr hWnd)
+        {
+            _currentHWnd = hWnd;
+            GetWindowRect(hWnd, out _currentRect );
+        }
+
+        public void SetUIDispatcher(Dispatcher ui)
         {
             _uiDispatcher = ui;
         }
 
-        public (long,long) GetCaretPosition()
+        public void GetCaretPosition()
         {
             IntPtr hWnd = GetForegroundWindow();
+
+            if(hWnd == _currentHWnd)
+            {
+                return;
+            }
+
             if (null != hWnd )
             {
-                Position pos;
-                GetCaretPos(out pos);
-                return (pos.x, pos.y);
+                GUITHREADINFO guiInfo = new GUITHREADINFO();
+                guiInfo.cbSize = Marshal.SizeOf(guiInfo);
+                GetGUIThreadInfo(0, ref guiInfo);
+
+                Position pos = new Position
+                {
+                    x = guiInfo.rcCaret.left,
+                    y = guiInfo.rcCaret.top
+                };
+                if (0 != pos.x && 0 != pos.y)
+                {
+                    ClientToScreen(hWnd,ref pos);
+                    MoveWindow(_currentHWnd,(int) pos.x+80
+                               , (int)pos.y - 15
+                               , _currentRect.right - _currentRect.left
+                               , _currentRect.bottom - _currentRect.top
+                               , true);
+                }
+                else
+                { 
+                    GetWindowRect(hWnd, out WinRect winRect );
+                    MoveWindow(_currentHWnd, winRect.right-100
+                               , winRect.bottom-50
+                               , _currentRect.right - _currentRect.left
+                               , _currentRect.bottom - _currentRect.top
+                               , true);
+                }
             }
-            return (0, 0);
+            return;
         }
 
         public void Dispose()
@@ -128,17 +271,11 @@ namespace Faster
         }
     }
 
-    public struct Position
-    {
-        public long x;
-        public long y;
-    }
-
     public class WPMArgs: EventArgs
     {
-        public int WPM { get; }
+        public double WPM { get; }
 
-        public WPMArgs(int wpm)
+        public WPMArgs(double wpm)
         {
             WPM = wpm;
         }
